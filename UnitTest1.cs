5cba2bacda64951771b8dcd067c4f2f696be8526
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-12-24 22:38:54 +0300

    Добавьте файлы проекта.

---
+++ UnitTest1.cs
@@ -0,0 +1,23 @@
+using MyServer;
+using System.Net;
+
+namespace ServerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMessageMethod()
+        {
+            var messageSource = new Mock<IMessageSource>();
+            var server = new Server(messageSource.Object);
+            server.Clients.Add("User1", new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+            var testMessage = new NetMessage { NickNameFrom = "User2", NickNameTo = "User1", Text = "Test Message" };
+            server.RelyMessage(testMessage).Wait();
+            messageSource.Verify(mock => mock.SendAsync(testMessage, It.IsAny<IPEndPoint>()), Times.Once);
+        }
+
+
+    }
+}
